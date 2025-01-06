ca958e6834645a7bd18dec82b9267f7eda8370f4
root <marcus.arruda@onlineapp.com.br>
2025-01-06 15:25:02 -0300

    too slow

--- adv_2024_12_07/src/main.rs
+++ adv_2024_12_07/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     first();
+    second();
 }
 
 fn first() {
@@ -18,7 +19,32 @@ fn first() {
         let n = data.len() - 2;
         let cur = String::new();
 
-        for op in operators(n, cur) {
+        for op in operators(n, cur, false) {
+            if evaluate(&data[1..], op, data[0]) {
+                result += data[0];
+                break;
+            }
+        }
+    }
+
+    println!("{}", result);
+}
+
+fn second() {
+    let mut result = 0;
+
+    for line in fs::read_to_string("data.txt").unwrap().lines() {
+        let d: Result<Vec<i64>, _> = line
+            .split_terminator(&[':', ' '][..])
+            .filter(|item| !item.is_empty())
+            .map(str::parse)
+            .collect();
+
+        let data = d.unwrap();
+        let n = data.len() - 2;
+        let cur = String::new();
+
+        for op in operators(n, cur, true) {
             if evaluate(&data[1..], op, data[0]) {
                 result += data[0];
                 break;
@@ -42,6 +68,11 @@ fn evaluate(data: &[i64], op: String, exp: i64) -> bool {
             }
             '*' => {
                 data[index + 1] = apply(data[index], data[index + 1], |x, y| x * y);
+            }
+            '|' => {
+                data[index + 1] = format!("{}{}", data[index], data[index + 1])
+                    .parse()
+                    .unwrap();
             }
             _ => {}
         }
@@ -67,16 +98,22 @@ where
     operation(a, b)
 }
 
-fn operators(size: usize, curr: String) -> Vec<String> {
+fn operators(size: usize, curr: String, add_concat: bool) -> Vec<String> {
     if size == curr.len() {
         return vec![curr];
     }
 
     let mut opers = Vec::new();
 
-    for c in ['+', '*'] {
+    let chars: &[char] = if add_concat {
+        &['+', '*', '|']
+    } else {
+        &['+', '*']
+    };
+
+    for c in chars {
         let data = format!("{}{}", curr, c);
-        let mut next_res = operators(size, data);
+        let mut next_res = operators(size, data, add_concat);
         opers.append(&mut next_res);
     }
 
@@ -92,7 +129,7 @@ mod tests {
         let n = 2;
         let cur = String::new();
 
-        let result = operators(n, cur);
+        let result = operators(n, cur, false);
         assert_eq!(result, ["++", "+*", "*+", "**"]);
     }
 
